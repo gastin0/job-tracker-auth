07c9c7fb284b37bec9dfc9982b257be1db555992
gastino <andre.abdirrosyid@gmail.com>
2026-02-10 00:40:56 +0700

    feat(ui) polish login page UI

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -32,36 +32,63 @@ export default function AdmminLoginPage() {
     }
 
     return (
-        <div>
-            <h1>Administrator Login</h1>
+        <div className="min-h-screen flex items-center justify-center -mt-16">
+            <div className="w-3/4 max-w-xl rounded-lg shadow-md p-6">
+                <h1 className="text-xl font-bold text-blue-900 mb-6 text-center font-mono">
+                    Administrator Login
+                </h1>
 
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Admin Username"
-                    value={username}
-                    disabled={isSubmitting}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Admin Password"
-                    value={password}
-                    disabled={isSubmitting}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
+                <form onSubmit={handleSubmit}>
+                    <div className="space-y-1 text-center">
+                        <input
+                            type="text"
+                            placeholder="Admin Username"
+                            value={username}
+                            disabled={isSubmitting}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                            className="
+                        w-2/3 rounded-md border px-3 py-2
+                        focus:outline-none focus:ring-1 focus:ring-blue-500
+                        focus:border-blue-500 mb-4 font-mono
+                        "
+                        />
+                    </div>
+                    <div className="space-y-1 text-center">
+                        <input
+                            type="password"
+                            placeholder="Admin Password"
+                            value={password}
+                            disabled={isSubmitting}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            className="
+                        w-2/3 rounded-md border px-3 py-2
+                        focus:outline-none focus:ring-1 focus:ring-blue-500
+                        focus:border-blue-500 mb-4 font-mono
+                        "
+                        />
+                    </div>
 
-                <button
-                    type="submit"
-                    onClick={() => redirect("/admin/applications")}
-                >
-                    Log In
-                </button>
+                    <div className="text-center">
 
-            
-            </form>
+                        <button
+                            type="submit"
+                            onClick={() => redirect("/admin/applications")}
+                            className="
+                            rounded-md px-4 py-2 font-mono
+                            font-medium text-center border-1
+                            border-blue-900 text-blue-900
+                        "
+                        >
+                            Log In
+                        </button>
+                    </div>
+
+
+                </form>
+
+            </div>
         </div>
     );
 }
